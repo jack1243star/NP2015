969fb8d1a9060b33970cd5067eab8e95d1fa9829
Jack Force <jack1243star@gmail.com>
2015-05-24 16:05:42 +0000

    Final version on iLMS

--- HW2_101062116/HW2_101062116_Cli.c
+++ HW2_101062116/HW2_101062116_Cli.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define MAXLINE 128
-#define IDOFFSET 1
-#define MAXIDLEN 20
+#define PACKETSIZE 1024
+#define ACKSIZE 1
+
+#define IDOFFSET 32
 #define PWOFFSET 64
-#define MAXPWLEN 20
+#define MAXIDLEN 16
+#define MAXPWLEN 16
 
 #define OP_NEWUSER   1
 #define OP_LOGIN     2
@@ -27,71 +31,180 @@
 #define OP_DELTEXT  15
 #define OP_DELUSER  16
 
-void dg_cli(FILE *fp, int sockfd, struct sockaddr *pservaddr, socklen_t servlen);
+void client(void);
+void alnum_prefix(char *str);
+int rawsend(void *buf, size_t size);
+int rawrecv(void *buf, size_t size);
+void acksend(void *buf, size_t size);
+void ackrecv(void *buf, size_t size);
+
+int sockfd;
+struct sockaddr_in servaddr;
 
 int main(int argc, char* argv[])
 {
-  int sockfd;
-  short servport;
-  struct sockaddr_in servaddr;
-
+  /* Check command line argument */
   if (argc!=3) {
     printf("Usage: %s <IPaddress> <Port>\n", argv[0]);
     return 0;
   }
 
-  servport = atoi(argv[2]);
-
+  /* Create UDP socket */
+  sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+  /* Initialize address structure */
   bzero(&servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(servport);
   inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  servaddr.sin_port = htons(atoi(argv[2]));
 
-  dg_cli(stdin, sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-  return 0;
+  /* Start client */
+  client();
+
+  return EXIT_SUCCESS;
 }
 
-void dg_cli(FILE *fp, int sockfd, struct sockaddr *pservaddr, socklen_t servlen)
+void client(void)
 {
-  int n;
-  char sendline[MAXLINE], recvline[MAXLINE+1];
+  char buf[PACKETSIZE];
 
+  printf("*** Welcome ***\n");
 welcome:
-  fputs("[R]egister [L]ogin\n", stdout);
-  fgets(sendline, MAXLINE, fp);
-  switch(sendline[0]) {
+  printf("[R]egister [L]ogin\n");
+  fgets(buf, 16, stdin);
+  switch(buf[0]) {
   case 'R': /* Register */
   case 'r':
-    memset(sendline, 0, MAXLINE);
-    sendline[0] = OP_NEWUSER;
+    memset(buf, 0, PACKETSIZE);
+    buf[0] = OP_NEWUSER;
 
     printf("ID(1-20 characters):");
-    fgets(sendline+IDOFFSET, MAXIDLEN, fp);
+    fgets(buf+IDOFFSET, MAXIDLEN, stdin);
+    alnum_prefix(buf+IDOFFSET);
 
     printf("PW(1-20 characters):");
-    fgets(sendline+PWOFFSET, MAXPWLEN, fp);
+    fgets(buf+PWOFFSET, MAXPWLEN, stdin);
+    alnum_prefix(buf+PWOFFSET);
 
-    sendto(sockfd, sendline, MAXLINE, 0, pservaddr, servlen);
-    n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+    acksend(buf, PACKETSIZE);
     break;
   case 'L': /* Login */
   case 'l':
-    memset(sendline, 0, MAXLINE);
-    sendline[0] = OP_LOGIN;
+    memset(buf, 0, PACKETSIZE);
+    buf[0] = OP_LOGIN;
 
     printf("ID(1-20 characters):");
-    fgets(sendline+IDOFFSET, MAXIDLEN, fp);
+    fgets(buf+IDOFFSET, MAXIDLEN, stdin);
+    alnum_prefix(buf+IDOFFSET);
 
     printf("PW(1-20 characters):");
-    fgets(sendline+PWOFFSET, MAXPWLEN, fp);
+    fgets(buf+PWOFFSET, MAXPWLEN, stdin);
+    alnum_prefix(buf+PWOFFSET);
 
-    sendto(sockfd, sendline, MAXLINE, 0, pservaddr, servlen);
-    n = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+    acksend(buf, PACKETSIZE);
+    ackrecv(buf, PACKETSIZE);
+
+    if (buf[0] == 1) {
+      printf("*** Hello %s ***\n", buf+IDOFFSET);
+      goto main;
+    } else {
+      printf("%s", buf+1);
+    }
     break;
   default:
     break;
   }
-
   goto welcome;
+
+main:
+  printf(
+    "[SU]Show User [SA]Show Article [A]dd Article [E]nter Article\n"
+    "[Y]ell [T]ell [L]ogout\n"
+    );
+  fgets(buf, 16, stdin);
+  switch(buf[0]) {
+  case 'S':
+  case 's':
+    switch(buf[1]) {
+    case 'U': /* Show user */
+    case 'u':
+      buf[0] = OP_SHOWUSER;
+      acksend(buf, PACKETSIZE);
+      for (;;) {
+        ackrecv(buf, PACKETSIZE);
+        if (buf[0] == 1)
+          printf("%s\n", buf+1);
+        else
+          break;
+      }
+      break;
+
+    case 'A': /* Show Article */
+    case 'a':
+      break;
+
+    default:
+      break;
+    }
+    break;
+
+  default:
+    break;
+  }
+  goto main;  
+}
+
+void alnum_prefix(char* str)
+{
+  unsigned int i = 0;
+  while (isalnum(str[i]))
+    i++;
+  str[i] = '\0';
+}
+
+int rawsend(void *buf, size_t size)
+{
+  return sendto(sockfd, buf, size,
+                0, (struct sockaddr *)(&servaddr), sizeof(servaddr));
+}
+
+int rawrecv(void *buf, size_t size)
+{
+  return recvfrom(sockfd, buf, size, 0, NULL, NULL);
+}
+
+void acksend(void *buf, size_t size)
+{
+  int n;
+  char ackbuf[ACKSIZE];
+  struct timeval tv;
+
+  /* Set recv timeout */
+  tv.tv_sec = 0;
+  tv.tv_usec = 100000;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv)) < 0) {
+    perror("Error");
+  }
+
+  for (;;) {
+    rawsend(buf, size);
+    n = rawrecv(ackbuf, ACKSIZE);
+    if (n > 0)
+      break;
+  }
+}
+
+void ackrecv(void *buf, size_t size)
+{
+  char ack = '\x06';
+  struct timeval tv;
+
+  /* Set recv timeout */
+  tv.tv_sec = 0;
+  tv.tv_usec = 0;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv)) < 0) {
+    perror("Error");
+  }
+
+  rawrecv(buf, size);
+  rawsend(&ack, ACKSIZE);
 }
--- HW2_101062116/HW2_101062116_Ser.c
+++ HW2_101062116/HW2_101062116_Ser.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
-#include <unistd.h>
-#include <errno.h>
+#include <string.h>
+
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
-#include <ctype.h>
 
-#define MAXLINE 128
-#define IDOFFSET 1
-#define MAXIDLEN 20
+#define PACKETSIZE 1024
+#define ACKSIZE 1
+
+#define IDOFFSET 32
 #define PWOFFSET 64
-#define MAXPWLEN 20
+#define MAXIDLEN 16
+#define MAXPWLEN 16
+
+#define MAXUSER 16
 
 #define OP_NEWUSER   1
 #define OP_LOGIN     2
@@ -31,77 +32,169 @@
 #define OP_DELTEXT  15
 #define OP_DELUSER  16
 
-void dg_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen);
-void prefix_alnum(char* str);
+struct user {
+  char id[16];
+  char pw[16];
+  int online;
+};
+
+void server(void);
+int rawsend(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len);
+int rawrecv(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len);
+void acksend(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len);
+void ackrecv(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len);
+
+int sockfd;
+struct sockaddr_in servaddr;
 
 int main(int argc, char* argv[])
 {
-  int sockfd;
-  short servport;
-  struct sockaddr_in servaddr, cliaddr;
-
+  /* Check command line argument */
   if (argc!=2) {
     printf("Usage: %s <Port>\n", argv[0]);
     return 0;
   }
 
-  servport = atoi(argv[1]);
-
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  /* Create UDP socket */
+  sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+  /* Initialize address structure */
   bzero(&servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(servport);
+  servaddr.sin_port = htons(atoi(argv[1]));
 
+  /* Bind to address */
   bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
-  dg_echo(sockfd, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+  /* Start server */
+  server();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
 
-void dg_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen)
+void server(void)
 {
-  int n;
-  socklen_t len;
-  char mesg[MAXLINE];
+  int i;
+  char buf[PACKETSIZE];
+  struct sockaddr_in cliaddr;
+  struct user users[MAXUSER];
 
-  len = clilen;
-  for(;;) {
-    n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
-    switch(mesg[0]) {
-    case OP_NEWUSER: /* Register */
-      prefix_alnum(mesg+IDOFFSET);
-      prefix_alnum(mesg+PWOFFSET);
-      printf("IP=%s PORT=%d OP_NEWUSER ID=%s PW=%s\n",
-	     inet_ntoa(((struct sockaddr_in*)pcliaddr)->sin_addr),
-	     htons(((struct sockaddr_in*)pcliaddr)->sin_port),
-	     mesg+IDOFFSET, mesg+PWOFFSET);
-      sendto(sockfd, mesg, MAXLINE, 0, pcliaddr, clilen);
+  for (i=0; i<MAXUSER; i++) {
+    memset(users[i].id, 0, MAXIDLEN);
+    memset(users[i].pw, 0, MAXPWLEN);
+    users[i].online = 0;
+  }
+
+  for (;;) {
+    ackrecv(buf, PACKETSIZE, (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
+    printf("Receive from IP=%s PORT=%d\n",
+           inet_ntoa(cliaddr.sin_addr),
+           htons(cliaddr.sin_port));
+    switch (buf[0]) {
+    case OP_NEWUSER:
+      for (i=0; i<MAXUSER; i++) {
+        if (users[i].id[0] == 0) {
+          strcpy(users[i].id, buf+IDOFFSET);
+          strcpy(users[i].pw, buf+PWOFFSET);
+          printf("Registered new user: %s\n", users[i].id);
+          break;
+        }
+        if (i == MAXUSER-1)
+          printf("Error: No more space for new user!\n");
+      }
       break;
-    case OP_LOGIN: /* Register */
-      prefix_alnum(mesg+IDOFFSET);
-      prefix_alnum(mesg+PWOFFSET);
-      printf("IP=%s PORT=%d OP_LOGIN ID=%s PW=%s\n",
-	     inet_ntoa(((struct sockaddr_in*)pcliaddr)->sin_addr),
-	     htons(((struct sockaddr_in*)pcliaddr)->sin_port),
-	     mesg+IDOFFSET, mesg+PWOFFSET);
-      sendto(sockfd, mesg, MAXLINE, 0, pcliaddr, clilen);
+    case OP_LOGIN:
+      printf("LOGIN\n");
+      for (i=0; i<MAXUSER; i++) {
+        if (strncmp(users[i].id, buf+IDOFFSET, MAXIDLEN) != 0) {
+          if (i == MAXUSER-1) {
+            /* ID incorrect */
+            buf[0] = 0;
+            strcpy(buf+1, "Incorrect ID\n");
+            acksend(buf, PACKETSIZE,
+                    (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
+            break;
+          } else {
+            continue;
+          }
+        }
+        if (strncmp(users[i].pw, buf+PWOFFSET, MAXIDLEN) != 0) {
+          /* PW incorrect */
+          buf[0] = 0;
+          strcpy(buf+1, "Incorrect PW\n");
+          acksend(buf, PACKETSIZE,
+                  (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
+          break;
+        } else {
+          /* Login success */
+          users[i].online = 1;
+          buf[0] = 1;
+          break;
+        }
+      }
+    case OP_SHOWUSER:
+      for (i=0; i<MAXUSER; i++) {
+        if (users[i].online == 1) {
+          buf[0] = 1;
+          strcpy(buf+1, users[i].id);
+          acksend(buf, PACKETSIZE,
+                  (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
+        }
+      }
+      buf[0] = 0;
+      acksend(buf, PACKETSIZE,
+              (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
       break;
     default:
-      printf("Receive from IP=%s PORT=%d\n",
-	     inet_ntoa(((struct sockaddr_in*)pcliaddr)->sin_addr),
-	     htons(((struct sockaddr_in*)pcliaddr)->sin_port));
-      sendto(sockfd, mesg, MAXLINE, 0, pcliaddr, clilen);
       break;
     }
   }
 }
 
-void prefix_alnum(char* str)
+int rawsend(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len)
 {
-  unsigned int i = 0;
-  while (isalnum(str[i]))
-    i++;
-  str[i] = '\0';
+  return sendto(sockfd, buf, size, 0, pcliaddr, len);
+}
+
+
+int rawrecv(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len)
+{
+  return recvfrom(sockfd, buf, size, 0, pcliaddr, &len);
+}
+
+void acksend(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len)
+{
+  int n;
+  char ackbuf[ACKSIZE];
+  struct timeval tv;
+
+  /* Set recv timeout */
+  tv.tv_sec = 0;
+  tv.tv_usec = 100000;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv)) < 0) {
+    perror("Error");
+  }
+
+  for (;;) {
+    rawsend(buf, size, pcliaddr, len);
+    n = rawrecv(ackbuf, ACKSIZE, pcliaddr, len);
+    if (n > 0)
+      break;
+  }
+}
+
+void ackrecv(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len)
+{
+  char ack = '\x06';
+  struct timeval tv;
+
+  /* Set recv timeout */
+  tv.tv_sec = 0;
+  tv.tv_usec = 0;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv)) < 0) {
+    perror("Error");
+  }
+
+  rawrecv(buf, size, pcliaddr, len);
+  rawsend(&ack, ACKSIZE, pcliaddr, len);
 }
