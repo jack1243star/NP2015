f7ac8c74ac06112ecc94d762f816314e19a82ff7
Jack Force <jack1243star@gmail.com>
2015-07-06 20:51:55 -0700

    Add tell function to HW2

--- HW2_101062116/HW2_101062116_Cli.c
+++ HW2_101062116/HW2_101062116_Cli.c
@@ -1,42 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <ctype.h>      /* isalnum() */
 
 #include <arpa/inet.h>
-#include <sys/socket.h>
+#include <sys/socket.h> /* Sockets */
+#include <unistd.h>     /* close() */
 
-#define PACKETSIZE 1024
-#define ACKSIZE 1
+/* POSIX threads */
+#include <pthread.h>
+/* Directory operations */
+#include <dirent.h>
+
+#define PACKETSIZE 512
+#define LISTENQ    10
+
+#define OP_NEWUSER  1
+#define OP_DELUSER  2
+#define OP_LOGIN    3
+#define OP_LOGOUT   4
+#define OP_DOWNLOAD 5
+#define OP_UPLOAD   6
+#define OP_TELL     7
+#define OP_YELL     8
+#define OP_SHOWUSER 9
+#define OP_SHOWFILE 10
+#define OP_GET      11
+#define OP_PUT      12
 
 #define IDOFFSET 32
 #define PWOFFSET 64
 #define MAXIDLEN 16
 #define MAXPWLEN 16
 
-#define OP_NEWUSER   1
-#define OP_LOGIN     2
-#define OP_SHOWUSER  3
-#define OP_SHOWLIST  4
-#define OP_ADDTEXT   5
-#define OP_SHOWTEXT  6
-#define OP_YELL      7
-#define OP_TELL      8
-#define OP_LOGOUT    9
-#define OP_RESPONSE 10
-#define OP_DOWNLOAD 11
-#define OP_UPLOAD   12
-#define OP_ADDBLIST 13
-#define OP_DELBLIST 14
-#define OP_DELTEXT  15
-#define OP_DELUSER  16
+#define IPOFFSET   128
+#define PORTOFFSET 192
+#define FNOFFSET   64
+#define MAXFNSIZE  128
 
 void client(void);
 void alnum_prefix(char *str);
-int rawsend(void *buf, size_t size);
-int rawrecv(void *buf, size_t size);
-void acksend(void *buf, size_t size);
-void ackrecv(void *buf, size_t size);
+int sendall(int sockfd, char *buf, size_t size);
+int recvall(int sockfd, char *buf, size_t size);
+int newsock(char **ip, short *port);
+void *listener(void *connfd);
+int sendfile(int sockfd, char* filename);
+int getfile(char* peer_ip, short peer_port, char* filename);
+int tell(char* peer_ip, short peer_port, char* msg);
 
 int sockfd;
 struct sockaddr_in servaddr;
@@ -49,13 +59,15 @@ int main(int argc, char* argv[])
     return 0;
   }
 
-  /* Create UDP socket */
-  sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+  /* Create TCP socket */
+  sockfd = socket(PF_INET, SOCK_STREAM, 0);
   /* Initialize address structure */
-  bzero(&servaddr, sizeof(servaddr));
+  memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
   servaddr.sin_port = htons(atoi(argv[2]));
+  /* Connect to server */
+  connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
   /* Start client */
   client();
@@ -66,6 +78,24 @@ int main(int argc, char* argv[])
 void client(void)
 {
   char buf[PACKETSIZE];
+  pthread_t listener_thread;
+  int listener_fd;
+  char* listener_ip;
+  short listener_port;
+  char peer_ip[64];
+  short peer_port;
+
+  /* Directory list stuffs */
+  DIR *dir;
+  struct dirent *ent;
+
+  /* Create listener thread */
+  listener_fd = newsock(&listener_ip, &listener_port);
+  if (pthread_create(&listener_thread, NULL, listener, &listener_fd)) {
+    printf("pthread error\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("Listening on PORT=%hu\n", listener_port);
 
   printf("*** Welcome ***\n");
 welcome:
@@ -85,7 +115,7 @@ welcome:
     fgets(buf+PWOFFSET, MAXPWLEN, stdin);
     alnum_prefix(buf+PWOFFSET);
 
-    acksend(buf, PACKETSIZE);
+    sendall(sockfd, buf, PACKETSIZE);
     break;
   case 'L': /* Login */
   case 'l':
@@ -100,11 +130,27 @@ welcome:
     fgets(buf+PWOFFSET, MAXPWLEN, stdin);
     alnum_prefix(buf+PWOFFSET);
 
-    acksend(buf, PACKETSIZE);
-    ackrecv(buf, PACKETSIZE);
+    sendall(sockfd, buf, PACKETSIZE);
+    recvall(sockfd, buf, PACKETSIZE);
 
     if (buf[0] == 1) {
       printf("*** Hello %s ***\n", buf+IDOFFSET);
+      /* Send file list */
+      dir = opendir(".");
+      while((ent = readdir(dir)) != NULL)
+      {
+        buf[0] = 1;
+	strcpy(buf+1, ent->d_name);
+        sendall(sockfd, buf, PACKETSIZE);
+      }
+      closedir(dir);
+      buf[0] = 0;
+      sendall(sockfd, buf, PACKETSIZE);
+      /* Send file server ip and port */
+      strcpy(buf+IPOFFSET, listener_ip);
+      memcpy(buf+PORTOFFSET, (void*)&listener_port, sizeof(short));
+      sendall(sockfd, buf, PACKETSIZE);
+      /* Goto main menu */
       goto main;
     } else {
       printf("%s", buf+1);
@@ -116,35 +162,88 @@ welcome:
   goto welcome;
 
 main:
-  printf(
-    "[SU]Show User [SA]Show Article [A]dd Article [E]nter Article\n"
-    "[Y]ell [T]ell [L]ogout\n"
-    );
+  printf("[SU]Show User [SF]Show File [T]ell [L]ogout\n"
+         "[D]ownload [U]pload\n");
   fgets(buf, 16, stdin);
   switch(buf[0]) {
+  case 'L':
+  case 'l':
+    buf[0] = OP_LOGOUT;
+    sendall(sockfd, buf, PACKETSIZE);
+    goto welcome;
+    break;
   case 'S':
   case 's':
     switch(buf[1]) {
     case 'U': /* Show user */
     case 'u':
       buf[0] = OP_SHOWUSER;
-      acksend(buf, PACKETSIZE);
+      sendall(sockfd, buf, PACKETSIZE);
       for (;;) {
-        ackrecv(buf, PACKETSIZE);
+        recvall(sockfd, buf, PACKETSIZE);
         if (buf[0] == 1)
-          printf("%s\n", buf+1);
+          printf("%16s %16s %5hu\n",
+		 buf+1, buf+IPOFFSET, *(short*)(buf+PORTOFFSET));
         else
           break;
       }
       break;
 
-    case 'A': /* Show Article */
-    case 'a':
+    case 'F': /* Show file */
+    case 'f':
+      buf[0] = OP_SHOWFILE;
+      sendall(sockfd, buf, PACKETSIZE);
+      for (;;) {
+        recvall(sockfd, buf, PACKETSIZE);
+        if (buf[0] == 1)
+          printf("%16s%48s\n", buf+1, buf+FNOFFSET);
+        else
+          break;
+      }
       break;
 
     default:
       break;
     }
+    break;
+
+  case 'D': /* Download file */
+  case 'd':
+    buf[0] = OP_DOWNLOAD;
+    printf("Input user ID:");
+    fgets(buf+IDOFFSET, 16, stdin);
+    alnum_prefix(buf+IDOFFSET);
+    printf("Input file name:");
+    fgets(buf+FNOFFSET, MAXFNSIZE, stdin);
+    buf[FNOFFSET+strlen(buf+FNOFFSET)-1]='\0';
+    printf("%s\n",buf+FNOFFSET);
+    sendall(sockfd, buf, PACKETSIZE);
+    /* Get IP and port */
+    recvall(sockfd, buf, PACKETSIZE);
+    /* Check for error */
+    if (buf[0] == 0) {printf("%s\n", buf+1); break;}
+    strcpy(peer_ip, buf+IPOFFSET);
+    memcpy(&peer_port, buf+PORTOFFSET, sizeof(short));
+    /* Go get file from peer */
+    getfile(peer_ip, peer_port, buf+FNOFFSET);
+    break;
+
+  case 'T':
+  case 't':
+    buf[0] = OP_TELL;
+    printf("Input user ID:");
+    fgets(buf+IDOFFSET, 16, stdin);
+    alnum_prefix(buf+IDOFFSET);
+    printf("Input message:");
+    fgets(buf+FNOFFSET, MAXFNSIZE, stdin);
+    buf[FNOFFSET+strlen(buf+FNOFFSET)-1]='\0';
+    /* Get IP and port */
+    sendall(sockfd, buf, PACKETSIZE);
+    recvall(sockfd, buf, PACKETSIZE);
+    /* Go get file from peer */
+    strcpy(peer_ip, buf+IPOFFSET);
+    memcpy(&peer_port, buf+PORTOFFSET, sizeof(short));
+    tell(peer_ip, peer_port, buf+FNOFFSET);
     break;
 
   default:
@@ -161,50 +260,202 @@ void alnum_prefix(char* str)
   str[i] = '\0';
 }
 
-int rawsend(void *buf, size_t size)
+int sendall(int sockfd, char *buf, size_t size)
 {
-  return sendto(sockfd, buf, size,
-                0, (struct sockaddr *)(&servaddr), sizeof(servaddr));
-}
-
-int rawrecv(void *buf, size_t size)
-{
-  return recvfrom(sockfd, buf, size, 0, NULL, NULL);
-}
-
-void acksend(void *buf, size_t size)
-{
+  size_t sentbyte = 0;
   int n;
-  char ackbuf[ACKSIZE];
-  struct timeval tv;
 
-  /* Set recv timeout */
-  tv.tv_sec = 0;
-  tv.tv_usec = 100000;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv)) < 0) {
-    perror("Error");
+  while (sentbyte < size) {
+    n = send(sockfd, buf+sentbyte, size-sentbyte, 0);
+    if (n == -1) return -1;
+    sentbyte += n;
   }
+
+  return 0;
+}
+
+int recvall(int sockfd, char *buf, size_t size)
+{
+  size_t recvbyte = 0;
+  int n;
+
+  while (recvbyte < size) {
+    n = recv(sockfd, buf+recvbyte, size-recvbyte, 0);
+    if (n == 0) return -1;
+    recvbyte += n;
+  }
+
+  return 0;
+}
+
+int newsock(char **ip, short *port)
+{
+  int sockfd;
+  struct sockaddr_in servaddr;
+  socklen_t servlen = sizeof(servaddr);
+
+  /* Create TCP socket */
+  sockfd = socket(PF_INET, SOCK_STREAM, 0);
+  /* Initialize address structure */
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = 0; /* Any socket */
+  /* Bind to address */
+  bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+  getsockname(sockfd, (struct sockaddr*)&servaddr, &servlen);
+  *ip = strdup(inet_ntoa(servaddr.sin_addr));
+  *port = ntohs(servaddr.sin_port);
+  /* Listen to socket */
+  listen(sockfd, LISTENQ);
+
+  return sockfd;
+}
+
+void *listener(void *arg)
+{
+  int listenfd, connfd;
+  struct sockaddr_in cliaddr;
+  size_t clilen;
+  char buf[PACKETSIZE];
+
+  listenfd = *(int*)arg;
 
   for (;;) {
-    rawsend(buf, size);
-    n = rawrecv(ackbuf, ACKSIZE);
-    if (n > 0)
+    clilen = sizeof(cliaddr);
+    /* Accept connection */
+    connfd = accept(listenfd, (struct sockaddr *)&cliaddr, (socklen_t *)&clilen);
+    printf("Connection from IP=%s PORT=%hu\n",
+	   inet_ntoa(cliaddr.sin_addr),
+	   ntohs(cliaddr.sin_port));
+    /* Handle request */
+    if(recvall(connfd, buf, PACKETSIZE)){continue;}
+    switch (buf[0]) {
+    case OP_TELL:
+	printf("%s\n", buf+FNOFFSET);
+	break;
+    case OP_GET:
+      if (sendfile(connfd, buf+FNOFFSET))
+	printf("Send file '%s' failed.\n", buf+FNOFFSET);
+      else
+	printf("Send file '%s' success.\n", buf+FNOFFSET);
       break;
+    case OP_PUT:
+      break;
+    default:
+      break;
+    }
   }
 }
 
-void ackrecv(void *buf, size_t size)
+int sendfile(int sockfd, char* filename)
 {
-  char ack = '\x06';
-  struct timeval tv;
+  FILE* file;
+  size_t leftsize, filesize, n;
+  char data[PACKETSIZE];
 
-  /* Set recv timeout */
-  tv.tv_sec = 0;
-  tv.tv_usec = 0;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv)) < 0) {
-    perror("Error");
+  /* Open file */
+  file = fopen(filename, "rb");
+  if(!file){printf("File not found: %s\n",filename);return -1;}
+  /* Calculate file size */
+  fseek(file, 0, SEEK_END);
+  filesize = ftell(file);
+  rewind(file);
+  /* Send file size */
+  sendall(sockfd, (void*)(&filesize), sizeof(long));
+
+  leftsize = filesize;
+  while (leftsize > 0) {
+    if (leftsize > PACKETSIZE) {
+      /* Send whole packet */
+      n = fread(data, 1, PACKETSIZE, file);
+      sendall(sockfd, data, PACKETSIZE);
+    } else {
+      /* Send last packet */
+      n = fread(data, 1, leftsize, file);
+      sendall(sockfd, data, leftsize);
+    }
+    leftsize -= n;
   }
+  fclose(file);
 
-  rawrecv(buf, size);
-  rawsend(&ack, ACKSIZE);
+  return 0;
 }
+
+int getfile(char* peer_ip, short peer_port, char* filename)
+{
+  int connfd;
+  struct sockaddr_in peeraddr;
+  FILE* file;
+  size_t leftsize, filesize, n;
+  char data[PACKETSIZE];
+
+  printf("Try to get '%s' from IP=%s PORT=%hu...", filename,
+	 peer_ip, peer_port);
+
+  /* Create TCP socket */
+  connfd = socket(PF_INET, SOCK_STREAM, 0);
+  /* Initialize address structure */
+  memset(&peeraddr, 0, sizeof(peeraddr));
+  peeraddr.sin_family = AF_INET;
+  inet_pton(AF_INET, peer_ip, &peeraddr.sin_addr);
+  peeraddr.sin_port = htons(peer_port);
+  /* Connect to peer */
+  connect(connfd, (struct sockaddr*)&peeraddr, sizeof(peeraddr));
+
+  /* Open file */
+  file = fopen(filename, "wb");
+  /* Send request */
+  data[0] = OP_GET;
+  strcpy(data+FNOFFSET, filename);
+  sendall(connfd, data, PACKETSIZE);
+  /* Recv file size */
+  recvall(connfd, (void*)(&filesize), sizeof(long));
+
+  leftsize = filesize;
+  while (leftsize > 0) {
+    if (leftsize > PACKETSIZE) {
+      /* Recv whole packet */
+      recvall(connfd, data, PACKETSIZE);
+      n = fwrite(data, 1, PACKETSIZE, file);
+    } else {
+      /* Send last packet */
+      recvall(connfd, data, leftsize);
+      n = fwrite(data, 1, leftsize, file);
+    }
+    leftsize -= n;
+  }
+  fclose(file);
+  close(connfd);
+  printf("Success.\n");
+  return 0;
+}
+
+int tell(char* peer_ip, short peer_port, char* msg)
+{
+  int connfd;
+  struct sockaddr_in peeraddr;
+  char data[PACKETSIZE];
+
+  printf("Tell '%s' to IP=%s PORT=%hu...", msg, peer_ip, peer_port);
+
+  /* Create TCP socket */
+  connfd = socket(PF_INET, SOCK_STREAM, 0);
+  /* Initialize address structure */
+  memset(&peeraddr, 0, sizeof(peeraddr));
+  peeraddr.sin_family = AF_INET;
+  inet_pton(AF_INET, peer_ip, &peeraddr.sin_addr);
+  peeraddr.sin_port = htons(peer_port);
+  /* Connect to peer */
+  connect(connfd, (struct sockaddr*)&peeraddr, sizeof(peeraddr));
+
+  /* Send request */
+  data[0] = OP_TELL;
+  strcpy(data+FNOFFSET, msg);
+  sendall(connfd, data, PACKETSIZE);
+
+  close(connfd);
+  printf("Success.\n");
+  return 0;
+}
+
--- HW2_101062116/HW2_101062116_Ser.c
+++ HW2_101062116/HW2_101062116_Ser.c
@@ -4,98 +4,141 @@
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
-#define PACKETSIZE 1024
-#define ACKSIZE 1
+#define LISTENQ     10
+#define PACKETSIZE  512
+#define NUM_THREADS 10
+#define MAXUSER     16
+
+#define OP_NEWUSER  1
+#define OP_DELUSER  2
+#define OP_LOGIN    3
+#define OP_LOGOUT   4
+#define OP_DOWNLOAD 5
+#define OP_UPLOAD   6
+#define OP_TELL     7
+#define OP_YELL     8
+#define OP_SHOWUSER 9
+#define OP_SHOWFILE 10
 
 #define IDOFFSET 32
 #define PWOFFSET 64
 #define MAXIDLEN 16
 #define MAXPWLEN 16
 
-#define MAXUSER 16
+#define IPOFFSET   128
+#define PORTOFFSET 192
+#define FNOFFSET   64
 
-#define OP_NEWUSER   1
-#define OP_LOGIN     2
-#define OP_SHOWUSER  3
-#define OP_SHOWLIST  4
-#define OP_ADDTEXT   5
-#define OP_SHOWTEXT  6
-#define OP_YELL      7
-#define OP_TELL      8
-#define OP_LOGOUT    9
-#define OP_RESPONSE 10
-#define OP_DOWNLOAD 11
-#define OP_UPLOAD   12
-#define OP_ADDBLIST 13
-#define OP_DELBLIST 14
-#define OP_DELTEXT  15
-#define OP_DELUSER  16
+struct file {
+  char filename[64];
+  struct file *prev;
+  struct file *next;
+};
 
 struct user {
   char id[16];
   char pw[16];
+  char ip[64];
+  short port;
   int online;
+  char listenip[64];
+  short listenport;
+  struct file *filelist;
 };
 
-void server(void);
-int rawsend(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len);
-int rawrecv(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len);
-void acksend(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len);
-void ackrecv(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len);
+struct session {
+  int connfd;
+  char ip[64];
+  short port;
+};
 
-int sockfd;
-struct sockaddr_in servaddr;
+void *serve(void *session);
+int sendall(int sockfd, char *buf, size_t size);
+int recvall(int sockfd, char *buf, size_t size);
+void addfile(struct user *u, char* filename);
+void freefile(struct file *root);
+
+/* Global user list */
+struct user users[MAXUSER];
 
 int main(int argc, char* argv[])
 {
+  int listenfd, connfd, i, j = 0;
+  struct sockaddr_in servaddr, cliaddr;
+  size_t clilen;
+  struct session *currsession;
+  pthread_t threads[NUM_THREADS];
+
   /* Check command line argument */
   if (argc!=2) {
     printf("Usage: %s <Port>\n", argv[0]);
     return 0;
   }
 
-  /* Create UDP socket */
-  sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+  /* Setup user list */
+  for (i=0; i<MAXUSER; i++) {
+    memset(users[i].id, 0, MAXIDLEN);
+    memset(users[i].pw, 0, MAXPWLEN);
+    users[i].online = 0;
+    users[i].filelist = NULL;
+  }
+
+
+  /* Create TCP socket */
+  listenfd = socket(PF_INET, SOCK_STREAM, 0);
   /* Initialize address structure */
   bzero(&servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(atoi(argv[1]));
-
   /* Bind to address */
-  bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+  bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+  /* Listen to socket */
+  listen(listenfd, LISTENQ);
 
-  /* Start server */
-  server();
+  for (;;) {
+    clilen = sizeof(cliaddr);
+    /* Accept connection */
+    connfd = accept(listenfd, (struct sockaddr *)&cliaddr, (socklen_t *)&clilen);
+    currsession = (struct session*)malloc(sizeof(struct session));
+    currsession->connfd = connfd;
+    /* Get IP */
+    strcpy(currsession->ip, inet_ntoa(cliaddr.sin_addr));
+    /* Get port number */
+    currsession->port = ntohs(cliaddr.sin_port);
+    /* Create client thread */
+    if (pthread_create(&threads[j++], NULL, serve, (void *)currsession)) {
+      printf("pthread error\n");
+      return EXIT_FAILURE;
+    }
+  }
 
   return EXIT_SUCCESS;
 }
 
-void server(void)
+void *serve(void *session)
 {
-  int i;
+  int connfd = ((struct session *)session)->connfd;
+  short port = ((struct session *)session)->port;
+  char ip[64];
   char buf[PACKETSIZE];
-  struct sockaddr_in cliaddr;
-  struct user users[MAXUSER];
+  int i, curruser;
+  struct file *tfile;
 
-  for (i=0; i<MAXUSER; i++) {
-    memset(users[i].id, 0, MAXIDLEN);
-    memset(users[i].pw, 0, MAXPWLEN);
-    users[i].online = 0;
-  }
+  strncpy(ip, ((struct session *)session)->ip, 64);
+  free(session);
 
   for (;;) {
-    ackrecv(buf, PACKETSIZE, (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
-    printf("Receive from IP=%s PORT=%d\n",
-           inet_ntoa(cliaddr.sin_addr),
-           htons(cliaddr.sin_port));
+    if(recvall(connfd, buf, PACKETSIZE)){printf("User disconnected.\n");return NULL;}
+    printf("Receive from IP=%s PORT=%hu\n", ip, port);
     switch (buf[0]) {
     case OP_NEWUSER:
       for (i=0; i<MAXUSER; i++) {
         if (users[i].id[0] == 0) {
-          strcpy(users[i].id, buf+IDOFFSET);
-          strcpy(users[i].pw, buf+PWOFFSET);
+          strncpy(users[i].id, buf+IDOFFSET, MAXIDLEN);
+          strncpy(users[i].pw, buf+PWOFFSET, MAXPWLEN);
           printf("Registered new user: %s\n", users[i].id);
           break;
         }
@@ -111,8 +154,7 @@ void server(void)
             /* ID incorrect */
             buf[0] = 0;
             strcpy(buf+1, "Incorrect ID\n");
-            acksend(buf, PACKETSIZE,
-                    (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
+            sendall(connfd, buf, PACKETSIZE);
             break;
           } else {
             continue;
@@ -122,28 +164,97 @@ void server(void)
           /* PW incorrect */
           buf[0] = 0;
           strcpy(buf+1, "Incorrect PW\n");
-          acksend(buf, PACKETSIZE,
-                  (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
+          sendall(connfd, buf, PACKETSIZE);
           break;
         } else {
           /* Login success */
+	  curruser = i;
           users[i].online = 1;
+	  strncpy(users[i].ip, ip, 64);
+	  users[i].port = port;
           buf[0] = 1;
+	  sendall(connfd, buf, PACKETSIZE);
+	  /* Receive file list */
+          for (;;) {
+            recvall(connfd, buf, PACKETSIZE);
+            if (buf[0] == 1) {
+              printf("%s\n", buf+1);
+	      addfile(&users[i], buf+1);
+	    } else
+	      break;
+	  }
+	  /* Receive listen IP and port */
+	  recvall(connfd, buf, PACKETSIZE);
+	  strncpy(users[i].listenip, buf+IPOFFSET, 64);
+	  memcpy(&users[i].listenport, buf+PORTOFFSET, sizeof(short));
           break;
         }
       }
+      break;
+    case OP_LOGOUT:
+      users[curruser].online = 0;
+      freefile(users[curruser].filelist);
+      break;
     case OP_SHOWUSER:
       for (i=0; i<MAXUSER; i++) {
         if (users[i].online == 1) {
           buf[0] = 1;
-          strcpy(buf+1, users[i].id);
-          acksend(buf, PACKETSIZE,
-                  (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
+          strncpy(buf+1, users[i].id, MAXIDLEN);
+	  strncpy(buf+IPOFFSET, users[i].ip, 64);
+	  memcpy(buf+PORTOFFSET, (void*)&users[i].port, sizeof(short));
+          sendall(connfd, buf, PACKETSIZE);
         }
       }
       buf[0] = 0;
-      acksend(buf, PACKETSIZE,
-              (struct sockaddr *)(&cliaddr), sizeof(cliaddr));
+      sendall(connfd, buf, PACKETSIZE);
+      break;
+    case OP_SHOWFILE:
+      for (i=0; i<MAXUSER; i++) {
+        if (users[i].online == 1) {
+	  tfile = users[i].filelist;
+	  while (tfile != NULL) {
+	    buf[0] = 1;
+	    strncpy(buf+1, users[i].id, MAXIDLEN);
+	    strncpy(buf+FNOFFSET, tfile->filename, 64);
+	    sendall(connfd, buf, PACKETSIZE);
+	    tfile = tfile->next;
+	  }
+        }
+      }
+      buf[0] = 0;
+      sendall(connfd, buf, PACKETSIZE);
+      break;
+    case OP_DOWNLOAD:
+      /* Send ip and port of peer which has the file */
+      for (i=0; i<MAXUSER; i++) {
+        if (strncmp(users[i].id, buf+IDOFFSET, MAXIDLEN) == 0) {
+	  buf[0] = 1;
+	  strcpy(buf+IPOFFSET, users[i].listenip);
+	  memcpy(buf+PORTOFFSET, &users[i].listenport, sizeof(short));
+	  sendall(connfd, buf, PACKETSIZE);
+	  break;
+        }
+      }
+      /* ID incorrect */
+      buf[0] = 0;
+      strcpy(buf+1, "Incorrect ID\n");
+      sendall(connfd, buf, PACKETSIZE);
+      break;
+    case OP_TELL:
+      /* Send ip and port of peer which has the file */
+      for (i=0; i<MAXUSER; i++) {
+        if (strncmp(users[i].id, buf+IDOFFSET, MAXIDLEN) == 0) {
+	  buf[0] = 1;
+	  strcpy(buf+IPOFFSET, users[i].listenip);
+	  memcpy(buf+PORTOFFSET, &users[i].listenport, sizeof(short));
+	  sendall(connfd, buf, PACKETSIZE);
+	  break;
+        }
+      }
+      /* ID incorrect */
+      buf[0] = 0;
+      strcpy(buf+1, "Incorrect ID\n");
+      sendall(connfd, buf, PACKETSIZE);
       break;
     default:
       break;
@@ -151,50 +262,63 @@ void server(void)
   }
 }
 
-int rawsend(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len)
+int sendall(int sockfd, char *buf, size_t size)
 {
-  return sendto(sockfd, buf, size, 0, pcliaddr, len);
-}
-
-
-int rawrecv(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len)
-{
-  return recvfrom(sockfd, buf, size, 0, pcliaddr, &len);
-}
-
-void acksend(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len)
-{
+  size_t sentbyte = 0;
   int n;
-  char ackbuf[ACKSIZE];
-  struct timeval tv;
 
-  /* Set recv timeout */
-  tv.tv_sec = 0;
-  tv.tv_usec = 100000;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv)) < 0) {
-    perror("Error");
+  while (sentbyte < size) {
+    n = send(sockfd, buf+sentbyte, size-sentbyte, 0);
+    if (n == -1) return -1;
+    sentbyte += n;
   }
 
-  for (;;) {
-    rawsend(buf, size, pcliaddr, len);
-    n = rawrecv(ackbuf, ACKSIZE, pcliaddr, len);
-    if (n > 0)
-      break;
+  return 0;
+}
+
+int recvall(int sockfd, char *buf, size_t size)
+{
+  size_t recvbyte = 0;
+  int n;
+
+  while (recvbyte < size) {
+    n = recv(sockfd, buf+recvbyte, size-recvbyte, 0);
+    if (n == 0) return -1;
+    recvbyte += n;
+  }
+
+  return 0;
+}
+
+void addfile(struct user *u, char* filename)
+{
+  struct file *cf, *pf, *new;
+
+  new = (struct file *)malloc(sizeof(struct file));
+  strcpy(new->filename, filename);
+  new->prev = NULL;
+  new->next = NULL;
+
+  if (u->filelist == NULL) {
+    u->filelist = new;
+  } else {
+    cf = u->filelist;
+    pf = NULL;
+    while (cf != NULL) {
+      pf = cf;
+      cf = cf->next;
+    }
+    pf->next = new;
+    new->prev = pf;
   }
 }
 
-void ackrecv(void *buf, size_t size, struct sockaddr *pcliaddr, socklen_t len)
+void freefile(struct file *root)
 {
-  char ack = '\x06';
-  struct timeval tv;
-
-  /* Set recv timeout */
-  tv.tv_sec = 0;
-  tv.tv_usec = 0;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv, sizeof(tv)) < 0) {
-    perror("Error");
+  struct file *curr = root;
+  while (curr->next != NULL) {
+    curr = curr->next;
+    free(curr->prev);
   }
-
-  rawrecv(buf, size, pcliaddr, len);
-  rawsend(&ack, ACKSIZE, pcliaddr, len);
+  free(curr);
 }
